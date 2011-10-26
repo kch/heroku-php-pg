2eb02ad35011b6f3d0640bc7e9a7a7abc3aeb65b
Caio Chassot <dev@caiochassot.com>
2011-10-26 15:06:14 +0200

    minor stuff

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php
-# this function reads your DATABASE_URL config var and returns a connection string suitable for pg_connect
+# This function reads your DATABASE_URL config var and returns a connection
+# string suitable for pg_connect. Put this in your app.
 function pg_connection_string_from_database_url() {
-  extract(parse_url($_ENV["DATABASE_URL"]), EXTR_SKIP);
-  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # you may want to add sslmode=require there too
+  extract(parse_url($_ENV["DATABASE_URL"]));
+  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
 }
 
-# here we establish the connection
+# Here we establish the connection. Yes, that's all.
 $pg_conn = pg_connect(pg_connection_string_from_database_url());
 
-# now let's use the connection for something silly just to prove it works
+# Now let's use the connection for something silly just to prove it works:
 $result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
 
 print "<pre>\n";
